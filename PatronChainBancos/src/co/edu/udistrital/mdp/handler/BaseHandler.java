aa065f6de2a1d534ab13f963476c7c159159b187
hailen <soyhailen@gmail.com>
2025-09-28 23:02:39 -0500

    Documentacion clase BaseHandler

--- PatronChainBancos/src/co/edu/udistrital/mdp/handler/BaseHandler.java
+++ PatronChainBancos/src/co/edu/udistrital/mdp/handler/BaseHandler.java
@@ -1,15 +1,21 @@
 package co.edu.udistrital.mdp.handler;
 
 import co.edu.udistrital.mdp.datamodel.CreditRequest;
-
+/**
+ * BaseHandler define la lógica común para todos los manejadores
+ * dentro de la cadena de responsabilidad.
+ */
 public abstract class BaseHandler implements Handler {
-    private Handler nextHandler;
+    private Handler nextHandler; // Siguiente manejador en la cadena
     
     @Override
     public void setNextHandler(Handler nextHandler) {
         this.nextHandler = nextHandler;
     }
-    
+        /**
+     * Pasa la solicitud al siguiente manejador si existe,
+     * de lo contrario la rechaza.
+     */
     protected void passToNext(CreditRequest request) {
         if (nextHandler != null) {
             nextHandler.handleRequest(request);
@@ -20,7 +26,13 @@ public abstract class BaseHandler implements Handler {
             System.out.println();
         }
     }
-    
+        /**
+     * Verifica si el manejador puede atender la solicitud.
+     */
     protected abstract boolean canHandle(CreditRequest request);
+        /**
+     * Procesa la solicitud si cumple con los criterios.
+     */
     protected abstract void processRequest(CreditRequest request);
+
 }
