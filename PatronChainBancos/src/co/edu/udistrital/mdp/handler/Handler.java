fd9b61f7c197339bdbff62d3c11d0b9cdcd5a354
hailen <soyhailen@gmail.com>
2025-09-28 22:57:51 -0500

    Documentacion clase Handler

--- PatronChainBancos/src/co/edu/udistrital/mdp/handler/Handler.java
+++ PatronChainBancos/src/co/edu/udistrital/mdp/handler/Handler.java
@@ -1,8 +1,18 @@
 package co.edu.udistrital.mdp.handler;
 
 import co.edu.udistrital.mdp.datamodel.CreditRequest;
-
+/**
+ * Handler define la estructura básica para la cadena de responsabilidad.
+ * Cada manejador puede procesar la solicitud o pasarla al siguiente.
+ */
 public interface Handler {
+        /**
+     * Asigna el siguiente manejador en la cadena.
+     */
     void setNextHandler(Handler nextHandler);
+        /**
+     * Procesa la solicitud de crédito o la reenvía si no aplica.
+     */
     void handleRequest(CreditRequest request);
 }
+
